f46db9acc18b8107cf963846623424f57cf506ff
Dovydas Rupsys <dovydas.rupsys@kcl.ac.uk>
2016-01-16 17:32:52 +0000

    Emailing service corrections for heroku server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'roomninja.com' }
+  config.action_mailer.default_url_options = { host: 'roomninja.herokuapp.com' }
 
   # SMTP settings for mailgun
   config.action_mailer.delivery_method = :smtp
